a21b467c9ebc94fab28c94003b5a214072689b28
jeongwon6955 <choirio2467@gmail.com>
2025-05-31 23:00:07 +0900

    .

--- vanilla_script/announcement/app.js
+++ vanilla_script/announcement/app.js
@@ -2,12 +2,12 @@ const login = document.querySelector(".login")
 const modal = document.querySelector(".modal")
 const closebtn = document.querySelector(".close > i")
 
-// 첫번째 로그인창 이벤트트
+// 첫번째 로그인창 이벤트
 login.addEventListener("click", function() {
     modal.classList.add("open-modal")
 })
 
-// 
+// 모델 창 닫는 이벤트 
 closebtn.addEventListener("click", function() {
     modal.classList.remove("open-modal")
 })
@@ -19,6 +19,7 @@ const side1 = document.querySelector(".sidebar1")
 const close1btn = document.querySelector(".close1 > i")
 const close2btn = document.querySelector(".close2 > i")
 
+// 두번째 로그인창 이벤트(사이드바1,2 여는 이벤트트)
 login1.addEventListener("click", function() {
 
     if(inputbox.value === "choijeongwon") {
@@ -32,9 +33,12 @@ login1.addEventListener("click", function() {
     }
 })
 
+// 두번째 사이드바 닫는 이벤트
 close1btn.addEventListener("click", function() {
     side1.classList.remove("show-sidebar1" )
 })
+
+// 첫번째 사이드바 닫는 이벤트
 close2btn.addEventListener("click", function() {
     side.classList.remove("show-sidebar")
 })
@@ -43,7 +47,7 @@ const todo = document.querySelector(".todo")
 const inputbtn = document.querySelector(".box > input")
 const btn = document.querySelector(".btn")
 
-
+// todolist 이벤트
 btn.addEventListener("click", function() {
     const list = document.createElement("li")
 
@@ -51,6 +55,7 @@ btn.addEventListener("click", function() {
         alert("경고!")
     }else {
         list.textContent = inputbtn.value
+        console.log(inputbtn.value);
         todo.appendChild(list)
         inputbtn.value = ""
     }
